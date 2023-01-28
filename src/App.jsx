c2454dc716ab88467d5d24ac7cc277aae48edf1d
Kuba Mrowiec <mruvekss@gmail.com>
2023-01-28 20:38:15 +0100

    Avax fetch data still needs fixing

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Navbar from './components/Navbar'
 
 import { Home, Stables, Yields, Fees, Bridges } from './pages/index'
 import Avax from './components/projects/Avax'
+import Search from './utilities/Search'
 
 function App() {
 
@@ -16,13 +17,14 @@ function App() {
       <div className="text-center text-white  py-10 text-4xl">
         Cross Chain Defi Platform
       </div>
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/stables" element={<Stables />} />
         <Route path="/yields" element={<Yields />} />
         <Route path="/fees" element={<Fees />} />
         <Route path="/bridges" element={<Bridges />} />
-        <Route path="/avax" element={<Avax protocol={`platypus-finance`}/>} />
+        <Route path="/avax" element={<Avax />} />
 
       </Routes>
     </div>
--- src/components/projects/Avax.jsx
+++ src/components/projects/Avax.jsx
@@ -1,22 +1,51 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 
-const Avax = ({ protocol }) => {
+import Loader from '../Loader';
 
+const Avax = () => {
+
+  const [protocols, setProtocols] = useState([]);
 
   useEffect(() => {
-    axios.get(`https://api.llama.fi/protocol/${protocol}`)
+    axios.get('https://api.llama.fi/protocols')
       .then((res) => {
-        console.log(res.data)
+        let result = [];
+        res.data.forEach((req) => {
+          if (req.options.chain === 'Avalanche') {
+            result.push(req)
+          }
+        })
+        this.setProtocols({ results: results })
+        console.log(protocols)
       })
       .catch((err) => {
         console.log(err)
       })
   }, [])
   return (
-    <div>
-      
+
+    <div className="h-max m-10 border rounded-xl border-gray-400 bg-gray-800 text-white">
+    <div className="grid grid-cols-7 p-2 border-black border-b ">
+      <header className="font-bold italic">Project</header>
+      <header className="text-right font-bold italic">Category</header>
+      <header className="text-right font-bold italic">Daily Fees</header>
+      <header className="text-right font-bold italic">Daily Holder Revenue</header>
+      <header className="text-right font-bold italic">Daily Protocol Revenue</header>
+      <header className="text-right font-bold italic">Revenue</header>
+      <header className="text-right font-bold italic">All time fees</header>
     </div>
+    {protocols ?
+      (
+        protocols.map(protocol =>
+          `${console.log(protocol.chain)}`
+
+          )
+      )
+        : (<Loader />)}
+
+
+  </div>
   )
 }
 
