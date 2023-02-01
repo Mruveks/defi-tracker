d155751cb263cd442ee8970855096134ba01ec2c
Kuba Mrowiec <mruvekss@gmail.com>
2023-02-01 21:17:36 +0100

    styled tvl chart

--- src/components/charts/TVLchart.jsx
+++ src/components/charts/TVLchart.jsx
@@ -17,32 +17,27 @@ const TVLchart = () => {
       })
   }, []);
 
+  const primaryxAxis = { valueType: 'Category', visible: false }
+  const primaryyAxis = { labelFormat: '${value}K', visible: false }
+  const legendSettings = { visible: true, textStyle: { color: 'white' } }
+  const tooltip = { enable: true, shared: false }
+
+  const palette = ["skyblue"]
 
 
-  const data = [
-    { month: 'Jan', sales: 35 }, { month: 'Feb', sales: 28 },
-    { month: 'Mar', sales: 34 }, { month: 'Apr', sales: 32 },
-    { month: 'May', sales: 40 }, { month: 'Jun', sales: 32 },
-    { month: 'Jul', sales: 35 }, { month: 'Aug', sales: 55 },
-    { month: 'Sep', sales: 38 }, { month: 'Oct', sales: 30 },
-    { month: 'Nov', sales: 25 }, { month: 'Dec', sales: 32 }
-  ];
-  
-  const primaryxAxis = { valueType: 'Category' }
-  const primaryyAxis = { labelFormat: '${value}K' };
-  const legendSettings = { visible: true };
-  const tooltip = { enable: true, shared: false };
 
   return (
-    <ChartComponent id="charts" primaryXAxis={primaryxAxis} primaryYAxis={primaryyAxis} legendSettings={legendSettings} tooltip={tooltip}>
+    <div className="flex justify-center">
+    <ChartComponent id="charts" primaryXAxis={primaryxAxis} primaryYAxis={primaryyAxis} palettes={palette} legendSettings={legendSettings} tooltip={tooltip}>
 
-      <Inject services={[ColumnSeries, Tooltip, LineSeries, Category, DateTime]} />
+      <Inject services={[ColumnSeries, Tooltip, LineSeries, DataLabel, Category, DateTime, Legend]} />
       
       <SeriesCollectionDirective>
-        <SeriesDirective dataSource={protocols} xName='date' yName='totalLiquidityUSD' name='TVL'/>
+        <SeriesDirective dataSource={protocols} xName='date' yName='totalLiquidityUSD' legendShape='Circle' name='Total Value Locked in USD'/>
       </SeriesCollectionDirective>
       
-    </ChartComponent>
+      </ChartComponent>
+    </div>
   )
 }
 
