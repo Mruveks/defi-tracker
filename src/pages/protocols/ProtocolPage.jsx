48c3d2bd23a4493078a3c3218522673e3c7fdc68
Kuba Mrowiec <mruvekss@gmail.com>
2023-04-09 11:37:55 +0200

    Fixed protocolData rendering bugs and added error handling

--- src/pages/protocols/ProtocolPage.jsx
+++ src/pages/protocols/ProtocolPage.jsx
@@ -33,7 +33,8 @@ const ProtocolPage = () => {
     <div key={raise.name}>
       <p>Total Raised: ${raise.amount}m</p>
       <p>
-        {moment.unix(raise.date).toDate()}: {raise.round} - Raised ${raise.amount}m
+        {moment.unix(raise.date).toDate().toLocaleString()}: {raise.round} -
+        Raised ${raise.amount}m
       </p>
     </div>
   );
@@ -50,7 +51,7 @@ const ProtocolPage = () => {
       ))}
     </div>
   );
-  console.log(protocolData)
+  console.log(protocolData);
 
   return (
     <main>
@@ -62,128 +63,207 @@ const ProtocolPage = () => {
         />
       </Helmet>
 
-      <div className="mt-10 mx-10">
+      <div className="mx-10">
         <SearchList />
       </div>
 
       <div className="grid grid-cols-2 mx-10 my-10 rounded-xl">
-        <div className="col-span-2 grid grid-cols-[40%_60%] border border-gray-600  rounded-xl">
-          {protocolData ? 
-          protocolData.map((protocol) => (
-            <div
-              key={protocol.id}
-              className="space-y-8 text-white w-[80%] p-4 italic capitalize"
-            >
-              <header className="text-3xl">
-                {protocolId} ({protocol.symbol})
-              </header>
-              <div className="grid gap-4">
-                <div>
-                  <h1>Total Value Locked</h1>
-                  <p>{numeral(tvl).format("$0.00a")}</p>
+        <div className="col-span-2 grid grid-cols-[25%_75%] border border-gray-600  rounded-xl">
+          {protocolData ? (
+            protocolData.map((protocol) => (
+              <div
+                key={protocol.id}
+                className="space-y-8 text-white w-[80%] p-4 italic capitalize"
+              >
+                <header className="text-3xl">
+                  {protocolId} ({protocol.symbol})
+                </header>
+                <div className="grid gap-4">
+                  <div>
+                    <h1>Total Value Locked</h1>
+                    <p>{numeral(tvl).format("$0.00a")}</p>
+                  </div>
+                  <div>
+                    <h1>Market Cap</h1>
+                    <p>{numeral(protocol.mcap).format("$0.00a")}</p>
+                  </div>
+                  <div>
+                    <h1>mcap/TVL</h1>
+                    <p>{(protocol.mcap / tvl).toFixed(2)}</p>
+                  </div>
                 </div>
                 <div>
-                  <h1>Market Cap</h1>
-                  <p>{numeral(protocol.mcap).format("$0.00a")}</p>
-                </div>
-                <div>
-                  <h1>mcap/TVL</h1>
-                  <p>{(protocol.mcap / tvl).toFixed(2)}</p>
+                  <p>Chain breakdown</p>
+                  <div className="flex space-x-4">
+                    {protocol.chainTvls.length > 1 ? (
+                      <p className="flex space-x-4 whitespace-pre-wrap">
+                        {protocol.chainTvls.map((chain) => (
+                          <div key={chain.id} className="flex">
+                            <h1>{chain}</h1>
+                            <p>{chain}</p>
+                          </div>
+                        ))}
+                      </p>
+                    ) : (
+                      <p>{protocol.chain}</p>
+                    )}
+                  </div>
                 </div>
               </div>
-              <div>
-                <p>Chain breakdown</p>
-                <div className="flex space-x-4">
-                  {protocol.chainTvls.length > 1 ? (
-                    <p className="flex space-x-4 whitespace-pre-wrap">
-                      {protocol.chainTvls.map((chain) => (
-                        <div key={chain.id} className="flex">
-                          <h1>{chain}</h1>
-                          <p>{chain}</p>
-                        </div>
-                      ))}
-                    </p>
-                  ) : (
-                    <p>{protocol.chain}</p>
-                  )}
-                </div>
-              </div>
-            </div>
-          )) : <div></div>}
+            ))
+          ) : (
+            <div></div>
+          )}
           <ProtocolsChart />
         </div>
 
-        <header className="mt-10 mb-2 text-2xl">Protocol Infromation</header>
-        { /** CODE BELOW IS BROKEN, CHANGING TO protocolDat ? BREAKS APP */}
-        {protocolData > 0 ?
-        protocolData.map((protocol) => (
-          <div
-            key={protocol.id}
-            className="space-y-4 col-span-2 p-4 w-full rounded-xl border border-gray-600"
-          >
-            <p className="text-justify">{protocol.description}</p>
-            <p>Category: {protocol.category}</p>
-            {protocol.listedAt ? (
-              <p>Created at: {moment.unix(protocol.listedAt).toDate()}</p>
-            ) : null}
-            {protocol.audit_links ? (
-              <div>
-                <h2>Audits:</h2>
-                {protocol.audit_links.map((audits) => (
-                  <div>{audits}</div>
-                ))}
+        <header className="mt-10 mb-2 text-2xl">Protocol Information</header>
+        {protocolData &&
+        (Array.isArray(protocolData)
+          ? protocolData.length > 0
+          : Object.keys(protocolData).length > 0) ? (
+          Array.isArray(protocolData) ? (
+            protocolData.map((protocol) => (
+              <div
+                key={protocol.id}
+                className="space-y-4 col-span-2 p-4 w-full rounded-xl border border-gray-600"
+              >
+                <p className="text-justify">{protocol.description}</p>
+                <p>Category: {protocol.category}</p>
+                {protocol.listedAt ? (
+                  <p>
+                    Created at:{" "}
+                    {moment.unix(protocol.listedAt).toDate().toLocaleString()}
+                  </p>
+                ) : null}
+                {protocol.audit_links ? (
+                  <div>
+                    <h2>Audits:</h2>
+                    {protocol.audit_links.map((audits) => (
+                      <a
+                        href={audits}
+                        target="__blank"
+                        className="hover:underline italic grid"
+                      >
+                        {audits}
+                      </a>
+                    ))}
+                  </div>
+                ) : null}
               </div>
-            ) : null}
-          </div>
-        )) : <div></div>}
+            ))
+          ) : (
+            <div
+              key={protocolData.id}
+              className="space-y-4 col-span-2 p-4 w-full rounded-xl border border-gray-600"
+            >
+              <p className="text-justify">{protocolData.description}</p>
+              <p>Category: {protocolData.category}</p>
+              {protocolData.listedAt ? (
+                <p>
+                  Created at:{" "}
+                  {moment.unix(protocol.listedAt).toDate().toLocaleString()}
+                </p>
+              ) : null}
+              {protocolData.audit_links ? (
+                <div>
+                  <h2>Audits:</h2>
+                  {protocolData.audit_links.map((audits) => (
+                    <div>{audits}</div>
+                  ))}
+                </div>
+              ) : null}
+            </div>
+          )
+        ) : (
+          <div>No protocol information available.</div>
+        )}
 
         <header className="mt-10 mb-2 text-2xl">Token Information</header>
-        {protocolData > 0 ?
-        protocolData.map((protocol) => (
-          <div
-            key={protocol.id}
-            className="col-span-2 space-y-4 p-4 w-full rounded-xl border border-gray-600"
-          >
-            <p>Address: {protocol.address}</p>
-            <div className="flex space-x-4">
-              <p>
-                <a
-                  href={`https:/www.coingecko.com/en/coins/${protocol.gecko_id}`}
-                  target="__blank"
-                  className="px-4 py-2 rounded bg-gray-900 w-fit hover:bg-gray-600"
-                >
-                  View on CoinGecko
-                </a>
-              </p>
-              <p>
-                <a
-                  href=""
-                  className="px-4 py-2 rounded bg-gray-900 w-fit hover:bg-gray-600"
-                >
-                  view on blockchain
-                </a>
-              </p>
+        {Array.isArray(protocolData) && protocolData.length > 0 ? (
+          protocolData.map((protocol) => (
+            <div
+              key={protocol.id}
+              className="col-span-2 space-y-4 p-4 w-full rounded-xl border border-gray-600"
+            >
+              <p>Address: {protocol.address}</p>
+              <div className="flex space-x-4">
+                <p>
+                  <a
+                    href={`https:/www.coingecko.com/en/coins/${protocol.gecko_id}`}
+                    target="__blank"
+                    className="px-4 py-2 rounded bg-gray-900 w-fit hover:bg-gray-600"
+                  >
+                    View on CoinGecko
+                  </a>
+                </p>
+                <p>
+                  <a
+                    href=""
+                    className="px-4 py-2 rounded bg-gray-900 w-fit hover:bg-gray-600"
+                  >
+                    view on blockchain
+                  </a>
+                </p>
+              </div>
             </div>
-          </div>
-        )) : <div></div>}
+          ))
+        ) : (
+          <div></div>
+        )}
 
-        {protocolData > 0 ? (
+        {protocolData && (
           <>
-            {protocolData.raises != 0 ? (
-              <>
-                <header className="mt-10 mb-2 text-2xl">Raises</header>
-                {protocolData.map((protocol) => (
+            {Array.isArray(protocolData)
+              ? protocolData.length > 0 &&
+                protocolData.map((protocol) => (
                   <div key={protocol.id} className="col-span-2">
+                    {protocol.raises && protocol.raises.length > 0 && (
+                      <>
+                        <header className="mt-10 mb-2 text-2xl">Raises</header>
+                        <div className="col-span-2 p-4 rounded-xl border border-gray-600">
+                          <div className="space-y-4">
+                            {protocol.raises.map(renderRaises)}
+                          </div>
+                        </div>
+                        <header className="mt-10 mb-2 text-2xl">
+                          Lead Investors
+                        </header>
+                        <div className="col-span-2 p-4 w-full rounded-xl border border-gray-600">
+                          {protocol.raises.map((raise) => (
+                            <div key={raise.name}>
+                              {renderInvestors(raise.leadInvestors)}
+                            </div>
+                          ))}
+                        </div>
+                        <header className="mt-10 mb-2 text-2xl">
+                          Other Investors
+                        </header>
+                        <div className="col-span-2 mb-10 p-4 w-full rounded-xl border border-gray-600">
+                          {protocol.raises.map((raise) => (
+                            <div key={raise.name}>
+                              {renderInvestors(raise.otherInvestors)}
+                            </div>
+                          ))}
+                        </div>
+                      </>
+                    )}
+                  </div>
+                ))
+              : protocolData.raises &&
+                protocolData.raises.length > 0 && (
+                  <>
+                    <header className="mt-10 mb-2 text-2xl">Raises</header>
                     <div className="col-span-2 p-4 rounded-xl border border-gray-600">
                       <div className="space-y-4">
-                        {protocol.raises.map(renderRaises)}
+                        {protocolData.raises.map(renderRaises)}
                       </div>
                     </div>
                     <header className="mt-10 mb-2 text-2xl">
                       Lead Investors
                     </header>
                     <div className="col-span-2 p-4 w-full rounded-xl border border-gray-600">
-                      {protocol.raises.map((raise) => (
+                      {protocolData.raises.map((raise) => (
                         <div key={raise.name}>
                           {renderInvestors(raise.leadInvestors)}
                         </div>
@@ -193,18 +273,16 @@ const ProtocolPage = () => {
                       Other Investors
                     </header>
                     <div className="col-span-2 mb-10 p-4 w-full rounded-xl border border-gray-600">
-                      {protocol.raises.map((raise) => (
+                      {protocolData.raises.map((raise) => (
                         <div key={raise.name}>
                           {renderInvestors(raise.otherInvestors)}
                         </div>
                       ))}
                     </div>
-                  </div>
-                ))}
-              </>
-            ) : null}
+                  </>
+                )}
           </>
-        ) : <div></div>}
+        )}
       </div>
     </main>
   );
