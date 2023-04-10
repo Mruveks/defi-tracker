d1833639d5d2fbe9dd0a6df3ac1d1738f503819f
Kuba Mrowiec <mruvekss@gmail.com>
2023-04-10 10:21:13 +0200

    Added support for more chains

--- src/utilities/ProtocolAddress.jsx
+++ src/utilities/ProtocolAddress.jsx
@@ -1,45 +1,53 @@
 import React from "react";
 
 const ProtocolAddress = ({ address }) => {
-  const [blockchain, addr] = address ? address.split(":") : [];
+
   let link;
 
-  switch (blockchain) {
-    case "eth":
-      link = `https://etherscan.io/address/${addr}`;
-      break;
-    case "bsc":
-      link = `https://bscscan.com/address/${addr}`;
-      break;
-    case "polygon":
-      link = `https://polygonscan.com/address/${addr}`;
-      break;
-    case "avax":
-      link = `https://cchain.explorer.avax.network/address/${addr}`;
-      break;
-    case "tron":
-      link = `https://tronscan.org/#/address/${addr}`;
-      break;
-    case "optimism":
-      link = `https://optimistic.etherscan.io/address/${addr}`;
-      break;
-    case "cronos":
-      link = `https://cronoscan.com/address/${addr}`;
-      break;
-    case "arbitrum":
-      link = `https://arbiscan.io/address/${addr}`;
-      break;
-    case "fantom":
-      link = `https://ftmscan.com/address/${addr}`;
-      break;
-    case "mixin":
-      if (/^0x[a-fA-F0-9]{34}$/.test(addr)) {
-        link = `https://mixin.one/snapshots/${addr}`;
-      }
-      break;
-    default:
-      link = null;
-      break;
+  if (address && address.includes(":")) {
+    const [blockchain, addr] = address.split(":");
+    switch (blockchain) {
+      case "eth":
+        link = `https://etherscan.io/address/${addr}`;
+        break;
+      case "bsc":
+        link = `https://bscscan.com/address/${addr}`;
+        break;
+      case "polygon":
+        link = `https://polygonscan.com/address/${addr}`;
+        break;
+      case "avax":
+        link = `https://cchain.explorer.avax.network/address/${addr}`;
+        break;
+      case "tron":
+        link = `https://tronscan.org/#/address/${addr}`;
+        break;
+      case "optimism":
+        link = `https://optimistic.etherscan.io/address/${addr}`;
+        break;
+      case "cronos":
+        link = `https://cronoscan.com/address/${addr}`;
+        break;
+      case "arbitrum":
+        link = `https://arbiscan.io/address/${addr}`;
+        break;
+      case "fantom":
+        link = `https://ftmscan.com/address/${addr}`;
+        break;
+      case "solana":
+        link = `https://explorer.solana.com/address/${addr}`;
+        break;
+      case "mixin":
+        if (/^0x[a-fA-F0-9]{34}$/.test(addr)) {
+          link = `https://mixin.one/snapshots/${addr}`;
+        }
+        break;
+      default:
+        link = `https://etherscan.io/address/${addr}`;
+        break;
+    }
+  } else {
+    link = `https://etherscan.io/address/${address}`
   }
 
   const handleClick = () => {
