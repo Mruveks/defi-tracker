1422246e2d042a6b287780bf2776849a2df606db
Kuba Mrowiec <mruvekss@gmail.com>
2023-05-17 16:11:30 +0200

    Bug fix

--- src/components/charts/CustomTooltip.jsx
+++ src/components/charts/CustomTooltip.jsx
@@ -6,7 +6,7 @@ const CustomTooltip = ({ active, payload, label }) => {
     const date = new Date(label);
     const formattedLabel = moment(date).format("YYYY-MM-DD");
     const formattedValue = numeral(payload[0].value).format("$0,0");
-    console.log(formattedLabel)
+
     return (
       <div className="flex w-full gap-4 lg:text-opacity-5 sm:w-0rem items-base items-baseline text-xl sm:text-lg bg-transparent ring-offset-0">
         <p className="flex font-mono text-right">{formattedLabel}</p>
