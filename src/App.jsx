04558e7932d2dbb4bb0ecdeefc4abecf0175fe7c
Kuba Mrowiec <mruvekss@gmail.com>
2023-05-25 08:36:39 +0200

    Bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ const StablecoinPage = lazy(() => import("./pages/stablecoins/StablecoinPage"));
 
 function App() {
 	return (
-		<div className="pl-48 pt-4 sm:pl-0 md:pl-0 md:mt-0 sm:mt-5 sm:mb-8">
+		<div className="pl-48 pt-4 sm:pl-0 md:pl-0 md:mt-0 sm:mt-5">
 			<Navbar />
 			<MobileNavbar />
 			<Suspense>
