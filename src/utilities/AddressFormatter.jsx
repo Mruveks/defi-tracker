f1a61d416e8a3fcf6de642c8400d9c9def5d8903
Kuba Mrowiec <mruvekss@gmail.com>
2023-05-25 09:57:22 +0200

    Added function to copy address

--- src/utilities/AddressFormatter.jsx
+++ src/utilities/AddressFormatter.jsx
@@ -1,30 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaRegCopy } from "react-icons/fa";
 
 const AddressFormatter = ({ address }) => {
-  let firstFour;
-  let lastFour;
+	let firstFour;
+	let lastFour;
 
-  const [chain, code] = address.split(":");
+	const [chain, code] = address.split(":");
+	const [copied, setCopied] = useState(false);
 
-  if (address && address.includes(":")) {
-    const [blockchain, addr] = address.split(":");
-    firstFour = addr.slice(0, 8);
-    lastFour = addr.slice(-8);
-  } else {
-    firstFour = address.slice(0, 8);
-    lastFour = address.slice(-8);
-  }
+	if (address && address.includes(":")) {
+		const [blockchain, addr] = address.split(":");
+		firstFour = addr.slice(0, 8);
+		lastFour = addr.slice(-8);
+	} else {
+		firstFour = address.slice(0, 8);
+		lastFour = address.slice(-8);
+	}
 
-  return (
-    <>
-      <span className="font-medium italic lg:block xl:block hidden font-mono">
-        {code ? code : address}
-      </span>
-      <span className=" italic xl:hidden lg:hidden visible font-mono">
-        {firstFour}...{lastFour}
-      </span>
-    </>
-  );
+	const handleCopy = () => {
+		navigator.clipboard.writeText(address);
+		setCopied(true);
+		setTimeout(() => {
+			setCopied(false);
+		}, 500);
+	};
+
+	return (
+		<>
+			<span className="font-medium italic lg:block xl:block hidden font-mono">
+				{code ? (
+					code
+				) : (
+					<div className="flex space-x-2 items-center">
+						<p>{address}</p>
+						<FaRegCopy
+							className={`text-gray-400 ${copied ? "" : "cursor-pointer"}`}
+							onClick={handleCopy}
+						/>
+						{copied && <span className="ml-1 text-gray-400">Copied!</span>}
+					</div>
+				)}
+			</span>
+			<span className="italic flex space-x-2 xl:hidden lg:hidden visible font-mono items-center">
+				<p>
+					{firstFour}...{lastFour}
+				</p>
+				<FaRegCopy
+					className={`text-gray-400 ${copied ? "" : "cursor-pointer"}`}
+					onClick={handleCopy}
+				/>
+				{copied && <span className="ml-1 text-gray-400">Copied!</span>}
+			</span>
+		</>
+	);
 };
 
 export default AddressFormatter;
