6478ff32c061ca95bbd1dc7efaf7f0030bb5a5f2
Kuba Mrowiec <mruvekss@gmail.com>
2023-06-29 19:45:01 +0200

    Delete unused buttons

--- src/components/rankings/TVLranking.jsx
+++ src/components/rankings/TVLranking.jsx
@@ -7,7 +7,6 @@ import { Link } from "react-router-dom";
 import { AiOutlineInfoCircle } from "react-icons/ai";
 const TVLranking = () => {
 	const [protocols, setProtocols] = useState([]);
-	const [query, setQuery] = useState("");
 
 	useEffect(() => {
 		axios
@@ -24,131 +23,8 @@ const TVLranking = () => {
 			});
 	}, []);
 
-	const buttonStyle = `px-4 bg-gray-700 rounded-lg text-md hover:bg-gray-600 transition duration-100`;
-
 	return (
 		<>
-			<div className="flex flex-wrap gap-4 justify-between h-full">
-				<button
-					className={`${buttonStyle} ${query === "" ? "bg-blue-600" : ""}`}
-					onClick={() => setQuery("")}
-				>
-					All
-				</button>
-
-				<button
-					className={`${buttonStyle} ${
-						query === "Ethereum" ? "bg-blue-600" : ""
-					}`}
-					onClick={() => setQuery("Ethereum")}
-				>
-					Ethereum
-				</button>
-
-				<button
-					className={`${buttonStyle} ${
-						query === "Avalanche" ? "bg-blue-600" : ""
-					}`}
-					onClick={() => setQuery("Avalanche")}
-				>
-					Avalanche
-				</button>
-
-				<button
-					className={`${buttonStyle} ${
-						query === "Binance" ? "bg-blue-600" : ""
-					}`}
-					onClick={() => setQuery("Binance")}
-				>
-					Binance
-				</button>
-
-				<button
-					className={`${buttonStyle} ${query === "Tron" ? "bg-blue-600" : ""}`}
-					onClick={() => setQuery("Tron")}
-				>
-					Tron
-				</button>
-
-				<button
-					className={`${buttonStyle} ${
-						query === "Arbitrum" ? "bg-blue-600" : ""
-					}`}
-					onClick={() => setQuery("Arbitrum")}
-				>
-					Arbitrum
-				</button>
-
-				<button
-					className={`${buttonStyle} ${
-						query === "Polygon" ? "bg-blue-600" : ""
-					}`}
-					onClick={() => setQuery("Polygon")}
-				>
-					Polygon
-				</button>
-
-				<button
-					className={`${buttonStyle} ${
-						query === "Cronos" ? "bg-blue-600" : ""
-					}`}
-					onClick={() => setQuery("Cronos")}
-				>
-					Cronos
-				</button>
-
-				<button
-					className={`${buttonStyle} ${
-						query === "Fantom" ? "bg-blue-600" : ""
-					}`}
-					onClick={() => setQuery("Fantom")}
-				>
-					Fantom
-				</button>
-
-				<button
-					className={`${buttonStyle} ${query === "Mixin" ? "bg-blue-600" : ""}`}
-					onClick={() => setQuery("Mixin")}
-				>
-					Mixin
-				</button>
-
-				<button
-					className={`${buttonStyle} ${
-						query === "DefiChain" ? "bg-blue-600" : ""
-					}`}
-					onClick={() => setQuery("DefiChain")}
-				>
-					DefiChain
-				</button>
-
-				<button
-					className={`${buttonStyle} ${
-						query === "Optimism" ? "bg-blue-600" : ""
-					}`}
-					onClick={() => setQuery("Optimism")}
-				>
-					Optimism
-				</button>
-
-				<button
-					className={`${buttonStyle} ${
-						query === "Solana" ? "bg-blue-600" : ""
-					}`}
-					onClick={() => setQuery("Solana")}
-				>
-					Solana
-				</button>
-				<button
-					className={`${buttonStyle} ${
-						query === "Bitcoin" ? "bg-blue-600" : ""
-					}`}
-					onClick={() => setQuery("Bitcoin")}
-				>
-					Bitcoin
-				</button>
-			</div>
-
 			<div className="h-max my-4 text-md border-gray-600 border p-2 rounded-xl bg-gray-850">
 				<div className="grid grid-cols-8 lg:grid-cols-6 sm:grid-cols-2 text-lg sm:text-sm font-semibold p-2 border-b-gray-600 text-right italic capitalize">
 					<header className="text-left">Name</header>
@@ -166,94 +42,11 @@ const TVLranking = () => {
 					</header>
 				</div>
 
-				{query === "" ? (
-					protocols.length ? (
-						protocols
-							.filter(
-								(item) =>
-									item.tvl != null &&
-									item.tvl >= 1000000 &&
-									item.category !== "CEX" &&
-									item.category !== "Chain"
-							)
-							.map((protocol, index) => (
-								<div
-									key={index}
-									className={` ${
-										index % 2 === 0 ? "bg-gray-800" : "bg-gray-850"
-									} grid grid-cols-8 md:grid-cols-6 lg:grid-cols-6 sm:grid-cols-2 items-center rounded-xl my-2 pr-2 text-right`}
-								>
-									<Link
-										to={`/protocol/${protocol.name}`}
-										className="flex items-center sm:space-x-0 space-x-4 w-full pl-2 py-2 text-left hover:bg-gray-600 transition duration-300 rounded-xl"
-									>
-										<div className="w-full flex items-center text-blue-400 space-x-2">
-											<img
-												src={protocol.logo}
-												alt="logo"
-												className="h-8 w-8 rounded-full "
-											/>
-											<p> {protocol.name}</p>
-										</div>
-									</Link>
-
-									<div className="sm:hidden md:hidden lg:hidden block">
-										{protocol.category}
-									</div>
-									<div className="sm:hidden md:hidden lg:hidden block">
-										{protocol.chain}
-									</div>
-
-									{protocol.change_1h > 0 ? (
-										<div className="text-green-500 sm:hidden block">
-											+{parseFloat(protocol.change_1h).toFixed(2)}%
-										</div>
-									) : (
-										<div className="text-red-500 sm:hidden block">
-											{parseFloat(protocol.change_1h).toFixed(2)}%
-										</div>
-									)}
-
-									{protocol.change_1d > 0 ? (
-										<div className="text-green-500 sm:hidden block">
-											+{parseFloat(protocol.change_1d).toFixed(2)}%
-										</div>
-									) : (
-										<div className="text-red-500 sm:hidden block">
-											{parseFloat(protocol.change_1d).toFixed(2)}%
-										</div>
-									)}
-
-									{protocol.change_7d > 0 ? (
-										<div className="text-green-500 sm:hidden block">
-											+{parseFloat(protocol.change_7d).toFixed(2)}%
-										</div>
-									) : (
-										<div className="text-red-500 sm:hidden block">
-											{parseFloat(protocol.change_7d).toFixed(2)}%
-										</div>
-									)}
-
-									{numeral(protocol.tvl).format("$0.00a")}
-
-									{protocol.mcap ? (
-										<div className="sm:hidden block">
-											{(protocol.mcap / protocol.tvl).toFixed(2)}
-										</div>
-									) : (
-										<div></div>
-									)}
-								</div>
-							))
-					) : (
-						<Loader />
-					)
-				) : protocols.length ? (
+				{protocols.length ? (
 					protocols
 						.filter(
 							(item) =>
 								item.tvl != null &&
-								item.chain === query &&
 								item.tvl >= 1000000 &&
 								item.category !== "CEX" &&
 								item.category !== "Chain"
@@ -262,12 +55,12 @@ const TVLranking = () => {
 							<div
 								key={index}
 								className={` ${
-									index % 2 === 0 ? "bg-[#222f3e]" : "bg-gray-800"
-								} grid grid-cols-8 md:grid-cols-6 lg:grid-cols-6 sm:grid-cols-2 items-center rounded-xl my-2 text-right`}
+									index % 2 === 0 ? "bg-gray-800" : "bg-gray-850"
+								} grid grid-cols-8 md:grid-cols-6 lg:grid-cols-6 sm:grid-cols-2 items-center rounded-xl my-2 pr-2 text-right`}
 							>
 								<Link
 									to={`/protocol/${protocol.name}`}
-									className="flex items-center sm:space-x-0 space-x-4 w-full px-2 sm:px-0 pl-2 py-2 text-left hover:bg-gray-600 transition duration-300 rounded-xl"
+									className="flex items-center sm:space-x-0 space-x-4 w-full pl-2 py-2 text-left hover:bg-gray-600 transition duration-300 rounded-xl"
 								>
 									<div className="w-full flex items-center text-blue-400 space-x-2">
 										<img
@@ -317,6 +110,7 @@ const TVLranking = () => {
 								)}
 
 								{numeral(protocol.tvl).format("$0.00a")}
+
 								{protocol.mcap ? (
 									<div className="sm:hidden block">
 										{(protocol.mcap / protocol.tvl).toFixed(2)}
--- src/components/rankings/YieldsRanking.jsx
+++ src/components/rankings/YieldsRanking.jsx
@@ -5,311 +5,108 @@ import Loader from "../Loader";
 import numeral from "numeral";
 import { Link } from "react-router-dom";
 const YieldsRanking = () => {
-  const [query, setQuery] = useState("");
-  const [Yields, setYields] = useState([]);
+	const [Yields, setYields] = useState([]);
 
-  useEffect(() => {
-    axios
-      .get("https://yields.llama.fi/pools")
-      .then((res) => {
-        setYields(res.data.data);
-        console.log(Yields);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
+	useEffect(() => {
+		axios
+			.get("https://yields.llama.fi/pools")
+			.then((res) => {
+				setYields(res.data.data);
+				console.log(Yields);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, []);
 
-  const buttonStyle = `px-4 rounded-lg text-md hover:bg-gray-600 transition duration-100`;
+	return (
+		<>
+			<div className="h-max my-4  text-md border rounded-xl bg-gray-850 border-gray-600 p-2">
+				<div className="grid grid-cols-7 sm:grid-cols-3 font-semibold p-2 text-lg sm:text-sm text-right capitalize italic">
+					<header className="text-left pl-2">Pool</header>
+					<header className="text-left pl-4">Project</header>
+					<header className="sm:hidden block">Chain</header>
+					<header>APY</header>
+					<header className="sm:hidden block">Base APY</header>
+					<header className="sm:hidden block">Reward APY</header>
+					<header className="sm:hidden block">TVL</header>
+				</div>
 
-  return (
-    <>
-      <div className="flex  flex-wrap gap-4 my-4 sm:my-10 sm:gap-2 h-fit xl:justify-between justify-items-stretch">
-        <button
-          className={`${buttonStyle} ${
-            query === "" ? "bg-blue-600" : "bg-gray-700"
-          }`}
-          onClick={() => setQuery("")}
-        >
-          All
-        </button>
+				{Yields.length ? (
+					Yields.filter(
+						(item) =>
+							item.apy != null && item.apy != "0" && item.tvlUsd >= 1000000
+					).map((pool, index) => (
+						<div
+							key={index}
+							className={` ${
+								index % 2 === 0 ? "bg-gray-800" : "bg-gray-850"
+							} grid grid-cols-7 sm:grid-cols-3 items-center rounded-xl p-2 text-right`}
+						>
+							<div className="flex space-x-4 sm:space-x-0 sm:p-0 text-left my-2 p-2 capitalize items-center">
+								<p>{pool.symbol.toLowerCase()}</p>
+							</div>
+							<Link
+								to={`/protocol/${pool.project}`}
+								className="flex xl:whitespace-nowrap items-center space-x-2 px-2 text-left hover:bg-gray-600 transition duration-300 rounded-xl"
+							>
+								<div className="md:w-40 px-2 capitalize my-auto text-blue-40">
+									{pool.project}
+								</div>
+							</Link>
+							<Link
+								to={
+									pool.chain === "BSC"
+										? `/chain/Binance`
+										: `/chain/${pool.chain}`
+								}
+								onClick={
+									pool.chain === "BSC"
+										? () => setActiveNav(`/Binance`)
+										: () => setActiveNav(`/${pool.chain}`)
+								}
+								className="flex sm:hidden w-fit justify-self-end justify-end items-center rounded-xl"
+							>
+								<div className="w-fit md:w-40 px-2 my-auto capitalize text-blue-400 hover:bg-gray-600 transition duration-100 rounded-xl">
+									{pool.chain}
+								</div>
+							</Link>
 
-        <button
-          className={`${buttonStyle} ${
-            query === "Ethereum" ? "bg-blue-600" : "bg-gray-700"
-          }`}
-          onClick={() => setQuery("Ethereum")}
-        >
-          Ethereum
-        </button>
+							{pool.apy ? (
+								<div className="font-mono">
+									{parseFloat(pool.apy).toFixed(2) + "%"}
+								</div>
+							) : (
+								<div></div>
+							)}
 
-        <button
-          className={`${buttonStyle} ${
-            query === "Avalanche" ? "bg-blue-600" : "bg-gray-700"
-          }`}
-          onClick={() => setQuery("Avalanche")}
-        >
-          Avalanche
-        </button>
+							{pool.apyBase ? (
+								<div className="sm:hidden block font-mono">
+									{parseFloat(pool.apyBase).toFixed(2) + "%"}
+								</div>
+							) : (
+								<div className="sm:hidden block"></div>
+							)}
 
-        <button
-          className={`${buttonStyle} ${
-            query === "BSC" ? "bg-blue-600" : "bg-gray-700"
-          }`}
-          onClick={() => setQuery("BSC")}
-        >
-          BSC
-        </button>
+							{pool.apyReward ? (
+								<div className="sm:hidden block font-mono">
+									{parseFloat(pool.apyReward).toFixed(2) + "%"}
+								</div>
+							) : (
+								<div className="sm:hidden block"></div>
+							)}
 
-        <button
-          className={`${buttonStyle} ${
-            query === "Tron" ? "bg-blue-600" : "bg-gray-700"
-          }`}
-          onClick={() => setQuery("Tron")}
-        >
-          Tron
-        </button>
-
-        <button
-          className={`${buttonStyle} ${
-            query === "Arbitrum" ? "bg-blue-600" : "bg-gray-700"
-          }`}
-          onClick={() => setQuery("Arbitrum")}
-        >
-          Arbitrum
-        </button>
-
-        <button
-          className={`${buttonStyle} ${
-            query === "Polygon" ? "bg-blue-600" : "bg-gray-700"
-          }`}
-          onClick={() => setQuery("Polygon")}
-        >
-          Polygon
-        </button>
-
-        <button
-          className={`${buttonStyle} ${
-            query === "Cronos" ? "bg-blue-600" : "bg-gray-700"
-          }`}
-          onClick={() => setQuery("Cronos")}
-        >
-          Cronos
-        </button>
-
-        <button
-          className={`${buttonStyle} ${
-            query === "Fantom" ? "bg-blue-600" : "bg-gray-700"
-          }`}
-          onClick={() => setQuery("Fantom")}
-        >
-          Fantom
-        </button>
-
-        <button
-          className={`${buttonStyle} ${
-            query === "Mixin" ? "bg-blue-600" : "bg-gray-700"
-          }`}
-          onClick={() => setQuery("Mixin")}
-        >
-          Mixin
-        </button>
-
-        <button
-          className={`${buttonStyle} ${
-            query === "DefiChain" ? "bg-blue-600" : "bg-gray-700"
-          }`}
-          onClick={() => setQuery("DefiChain")}
-        >
-          DefiChain
-        </button>
-
-        <button
-          className={`${buttonStyle} ${
-            query === "Optimism" ? "bg-blue-600" : "bg-gray-700"
-          }`}
-          onClick={() => setQuery("Optimism")}
-        >
-          Optimism
-        </button>
-
-        <button
-          className={`${buttonStyle} ${
-            query === "Solana" ? "bg-blue-600" : "bg-gray-700"
-          }`}
-          onClick={() => setQuery("Solana")}
-        >
-          Solana
-        </button>
-        <button
-          className={`${buttonStyle} ${
-            query === "Bitcoin" ? "bg-blue-600" : "bg-gray-700"
-          }`}
-          onClick={() => setQuery("Bitcoin")}
-        >
-          Bitcoin
-        </button>
-      </div>
-
-      <div className="h-max mb-8 text-md border rounded-xl bg-gray-850 border-gray-600 p-2">
-        <div className="grid grid-cols-7 sm:grid-cols-3 font-semibold p-2 text-lg sm:text-sm text-right capitalize italic">
-          <header className="text-left pl-2">Pool</header>
-          <header className="text-left pl-4">Project</header>
-          <header className="sm:hidden block">Chain</header>
-          <header>APY</header>
-          <header className="sm:hidden block">Base APY</header>
-          <header className="sm:hidden block">Reward APY</header>
-          <header className="sm:hidden block">TVL</header>
-        </div>
-
-        {query === "" ? (
-          Yields.length ? (
-            Yields.filter(
-              (item) =>
-                item.apy != null && item.apy != "0" && item.tvlUsd >= 1000000
-            ).map((pool, index) => (
-              <div
-                key={index}
-                className={` ${
-                  index % 2 === 0 ? "bg-gray-800" : "bg-gray-850"
-                } grid grid-cols-7 sm:grid-cols-3 items-center rounded-xl p-2 text-right`}
-              >
-                <div className="flex space-x-4 sm:space-x-0 sm:p-0 text-left p-2 capitalize items-center">
-                  <p>{pool.symbol.toLowerCase()}</p>
-                </div>
-                <Link
-                  to={`/protocol/${pool.project}`}
-                  className="flex items-center space-x-2 px-2 w-fit text-left hover:bg-gray-600 transition duration-300 rounded-xl"
-                >
-                  <div className="w-fit md:w-40 px-2 capitalize my-auto text-blue-400">
-                    {pool.project}
-                  </div>
-                </Link>
-                <Link
-                  to={
-                    pool.chain === "BSC"
-                      ? `/chain/Binance`
-                      : `/chain/${pool.chain}`
-                  }
-                  onClick={
-                    pool.chain === "BSC"
-                      ? () => setActiveNav(`/Binance`)
-                      : () => setActiveNav(`/${pool.chain}`)
-                  }
-                  className="flex sm:hidden w-full justify-end items-center rounded-xl"
-                >
-                  <div className="w-fit md:w-40 px-2 my-auto capitalize text-blue-400 hover:bg-gray-600 transition duration-100 rounded-xl">
-                    {pool.chain}
-                  </div>
-                </Link>
-
-                {pool.apy ? (
-                  <div className="font-mono">
-                    {parseFloat(pool.apy).toFixed(2) + "%"}
-                  </div>
-                ) : (
-                  <div></div>
-                )}
-
-                {pool.apyBase ? (
-                  <div className="sm:hidden block font-mono">
-                    {parseFloat(pool.apyBase).toFixed(2) + "%"}
-                  </div>
-                ) : (
-                  <div className="sm:hidden block"></div>
-                )}
-
-                {pool.apyReward ? (
-                  <div className="sm:hidden block font-mono">
-                    {parseFloat(pool.apyReward).toFixed(2) + "%"}
-                  </div>
-                ) : (
-                  <div className="sm:hidden block"></div>
-                )}
-
-                <div className="font-mono sm:hidden">
-                  {numeral(pool.tvlUsd).format("$0.00a")}
-                </div>
-              </div>
-            ))
-          ) : (
-            <Loader />
-          )
-        ) : Yields.length ? (
-          Yields.filter(
-            (item) =>
-              item.apy != null &&
-              item.apy != "0" &&
-              item.tvlUsd >= 1000000 &&
-              item.chain === query
-          ).map((pool, index) => (
-            <div
-              className={` ${
-                index % 2 === 0 ? "bg-[#222f3e]" : "bg-gray-800"
-              } grid grid-cols-7 sm:grid-cols-3 items-center p-2 rounded-xl text-right`}
-              key={index}
-            >
-              <div className="flex space-x-4 sm:space-x-0 sm:p-0 text-left p-2 capitalize items-center">
-                <p>{pool.symbol.toLowerCase()}</p>
-              </div>
-              <Link
-                to={`/protocol/${pool.project}`}
-                className="flex w-fit items-center text-left hover:bg-gray-600 transition duration-100 rounded-xl"
-              >
-                <div className="w-fit md:w-40 px-2 my-auto capitalize text-blue-400">
-                  {pool.project}
-                </div>
-              </Link>
-
-              <Link
-                to={
-                  pool.chain === "BSC"
-                    ? `/chain/Binance`
-                    : `/chain/${pool.chain}`
-                }
-                onClick={() => setActiveNav(`/${pool.chain}`)}
-                className="flex sm:hidden w-full justify-end items-center rounded-xl"
-              >
-                <div className="w-fit md:w-40 px-2 my-auto capitalize text-blue-400 hover:bg-gray-600 transition duration-100 rounded-xl">
-                  {pool.chain}
-                </div>
-              </Link>
-
-              {pool.apy ? (
-                <div className="font-mono">
-                  {parseFloat(pool.apy).toFixed(2) + "%"}
-                </div>
-              ) : (
-                <div></div>
-              )}
-
-              {pool.apyBase ? (
-                <div className="sm:hidden block font-mono">
-                  {parseFloat(pool.apyBase).toFixed(2) + "%"}
-                </div>
-              ) : (
-                <div></div>
-              )}
-
-              {pool.apyReward ? (
-                <div className="sm:hidden block font-mono">
-                  {parseFloat(pool.apyReward).toFixed(2) + "%"}
-                </div>
-              ) : (
-                <div></div>
-              )}
-
-              <div className="font-mono sm:hidden">
-                {numeral(pool.tvlUsd).format("$0.00a")}
-              </div>
-            </div>
-          ))
-        ) : (
-          <Loader />
-        )}
-      </div>
-    </>
-  );
+							<div className="font-mono sm:hidden">
+								{numeral(pool.tvlUsd).format("$0.00a")}
+							</div>
+						</div>
+					))
+				) : (
+					<Loader />
+				)}
+			</div>
+		</>
+	);
 };
 
 export default YieldsRanking;
