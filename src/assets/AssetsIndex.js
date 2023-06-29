3425c293f502cedccfbf62bfb1477ab99537ecab
Kuba Mrowiec <mruvekss@gmail.com>
2023-06-29 17:02:43 +0200

    Add more prices

--- src/assets/AssetsIndex.js
+++ src/assets/AssetsIndex.js
@@ -1,28 +1,41 @@
-import algo from './algorand.png'
-import arbitrum from './arbitrum.png'
-import eth from './ethereum.png'
-import bsc from './bsc.png'
-import avax from './avalanche.png'
-import polygon from './polygon.png'
-import optimism from './optimism.png'
-import solana from './solana.png'
-import kava from './kava.png'
-import ftm from './fantom.png'
-import tron from './tron.png'
+import algo from "./algorand.png";
+import arbitrum from "./arbitrum.png";
+import eth from "./ethereum.png";
+import bsc from "./bsc.png";
+import avax from "./avalanche.png";
+import polygon from "./polygon.png";
+import optimism from "./optimism.png";
+import solana from "./solana.png";
+import kava from "./kava.png";
+import ftm from "./fantom.png";
+import tron from "./tron.png";
 
-import flame from './flame.png'
+import curve from "./curve.png";
+import lido from "./lido.png";
+import aave from "./aave.png";
+import pancakeswap from "./pancakeswap.png";
+import sushi from "./sushi.png";
+import uniswap from "./uniswap.png";
+
+import flame from "./flame.png";
 
 export {
-  arbitrum,
-  eth,
-  bsc,
-  avax,
-  solana,
-  polygon,
-  optimism,
-  kava,
-  ftm,
-  tron,
-  algo,
-  flame
-}
+	arbitrum,
+	eth,
+	bsc,
+	avax,
+	solana,
+	polygon,
+	optimism,
+	kava,
+	ftm,
+	tron,
+	algo,
+	curve,
+	lido,
+	aave,
+	pancakeswap,
+	sushi,
+	uniswap,
+	flame,
+};
--- src/components/BackButton.jsx
+++ src/components/BackButton.jsx
@@ -3,11 +3,22 @@ import { useNavigate } from "react-router-dom";
 import { BsArrowLeft } from "react-icons/bs";
 import axios from "axios";
 import numeral from "numeral";
+import {
+	eth,
+	lido,
+	aave,
+	uniswap,
+	pancakeswap,
+	sushi,
+	curve,
+} from "../assets/AssetsIndex";
 function BackButton() {
 	const [ethPrice, setEthPrice] = useState(0);
 	const [lidoPrice, setLidoPrice] = useState(0);
 	const [uniPrice, setUniPrice] = useState(0);
 	const [aavePrice, setAavePrice] = useState(0);
+	const [curvePrice, setCurvePrice] = useState(0);
+	const [sushiPrice, setSushiPrice] = useState(0);
 
 	const navigate = useNavigate();
 
@@ -19,13 +30,15 @@ function BackButton() {
 		const fetchPrice = async () => {
 			try {
 				const response = await axios.get(
-					"https://coins.llama.fi/prices/current/coingecko:ethereum,coingecko:lido-dao,coingecko:uniswap,coingecko:aave,coingecko:curve-dao"
+					"https://coins.llama.fi/prices/current/coingecko:ethereum,coingecko:lido-dao,coingecko:uniswap,coingecko:aave,coingecko:curve-dao-token,coingecko:sushi"
 				);
 				const data = response.data.coins;
 				setEthPrice(data["coingecko:ethereum"].price);
 				setUniPrice(data["coingecko:uniswap"].price);
 				setLidoPrice(data["coingecko:lido-dao"].price);
 				setAavePrice(data["coingecko:aave"].price);
+				setSushiPrice(data["coingecko:sushi"].price);
+				setCurvePrice(data["coingecko:curve-dao-token"].price);
 			} catch (error) {
 				console.log(error);
 			}
@@ -43,10 +56,30 @@ function BackButton() {
 				<BsArrowLeft />
 				<p>Go Back</p>
 			</button>
-			<p>Ethereum: {numeral(ethPrice).format("$0,0.00")}</p>
-			<p>Uniswap: {numeral(uniPrice).format("$0,0.00")}</p>
-			<p>Lido: {numeral(lidoPrice).format("$0,0.00")}</p>
-			<p>Aave: {numeral(aavePrice).format("$0,0.00")}</p>
+			<p className="flex items-center">
+				<img src={eth} alt="" className="rounded-full h-5 w-5 mr-1" />
+				Ethereum: {numeral(ethPrice).format("$0,0.00")}
+			</p>
+			<p className="flex items-center">
+				<img src={uniswap} alt="" className="rounded-full h-5 w-5 mr-1" />
+				Uniswap: {numeral(uniPrice).format("$0,0.00")}
+			</p>
+			<p className="flex items-center">
+				<img src={lido} alt="" className="rounded-full h-5 w-5 mr-1" />
+				Lido: {numeral(lidoPrice).format("$0,0.00")}
+			</p>
+			<p className="flex items-center">
+				<img src={aave} alt="" className="rounded-full h-5 w-5 mr-1" />
+				Aave: {numeral(aavePrice).format("$0,0.00")}
+			</p>
+			<p className="flex items-center">
+				<img src={curve} alt="" className="rounded-full h-5 w-5 mr-1" />
+				Curve: {numeral(curvePrice).format("$0,0.00")}
+			</p>
+			<p className="flex items-center">
+				<img src={sushi} alt="" className="rounded-full h-5 w-5 mr-1" />
+				Sushi: {numeral(sushiPrice).format("$0,0.00")}
+			</p>
 		</div>
 	);
 }
