08dc4511f776830ba5860521196945fd57d56a29
Kuba Mrowiec <mruvekss@gmail.com>
2023-06-29 17:43:46 +0200

    Add new color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    screens: {
-      sm: { max: "767px" },
-      md: { min: "768px", max: "1023px" },
-      lg: { min: "1024px", max: "1279px" },
-      xl: { min: "1280px" },
-    },
-    extend: {},
-  },
-  plugins: [],
+	content: ["index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		screens: {
+			sm: { max: "767px" },
+			md: { min: "768px", max: "1023px" },
+			lg: { min: "1024px", max: "1279px" },
+			xl: { min: "1280px" },
+		},
+		extend: {
+			colors: {
+				"gray-850": "#18202F",
+			},
+		},
+	},
+	plugins: [],
 };
